deb87f6324748863948213a96cc25b6c7cb46e3c
AdrianBatalla <batalla.jose.fm@gmail.com>
2023-09-06 13:11:20 -0500

    Update EditSoft.java

--- EditSoft.java
+++ EditSoft.java
@@ -1,93 +1,167 @@
-import javax.swing.*;
-import javax.swing.UIManager.LookAndFeelInfo;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.filechooser.FileSystemView;
 
-public class EditSoft {
+import java.io.File;
+import javax.swing.JFileChooser;
 
-    public static void main(String[] args) {
+/**
+ *
+ * @author BATALLA
+ */
+public class EditSoft extends javax.swing.JFrame {
+
+    public EditSoft() {
+        initComponents();
+    }
+                     
+    private void initComponents() {
+
+        jFileChooser1 = new javax.swing.JFileChooser();
+        jButton2 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        jMenu3 = new javax.swing.JMenu();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(0, 0, 0));
+
+        jButton2.setText("Seleccionar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Porfavor seleccione una carpeta:");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 306, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 224, Short.MAX_VALUE)
+        );
+
+        jLabel2.setText("Error:");
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jMenu1.setText("Opciones");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Editar");
+        jMenuBar1.add(jMenu2);
+
+        jMenu3.setText("Ayuda");
+        jMenuBar1.add(jMenu3);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jButton2))
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 400, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(394, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 582, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+
+        pack();
+    }                    
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        int result = fileChooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedDirectory = fileChooser.getSelectedFile();
+            String directoryPath = selectedDirectory.getAbsolutePath();
+
+            jLabel1.setText("Directorio actual: "+directoryPath);
+            jTextArea1.setText("");
+
+            File[] files = selectedDirectory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isFile()) {
+                        jTextArea1.append("\u251C"+file.getName() + "\n");
+                    }
+                }
+            }
+        }
+    }                                        
+
+    public static void main(String args[]) {
         try {
-            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    UIManager.setLookAndFeel(info.getClassName());
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(EditSoft.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(EditSoft.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(EditSoft.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(EditSoft.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
 
-        // Create a window
-        JFrame ventana = new JFrame("Modificalo, pue");
-        ventana.setSize(800, 700);
-        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventana.getContentPane().setBackground(new Color(0x6D909F));
-        ventana.setLayout(new BorderLayout());
-
-        // Create a menu bar
-        JMenuBar menuBar = new JMenuBar();
-        JMenu opcionesMenu = new JMenu("Opciones");
-        JMenu editarMenu = new JMenu("Editar");
-        JMenu ayudaMenu = new JMenu("Ayuda");
-        // Create sub-menu items
-        JMenuItem ejemplo1 = new JMenuItem("Ejemplo1");
-        JMenuItem ejemplo11 = new JMenuItem("Ejemplo11");
-        JMenuItem ejemplo2 = new JMenuItem("Ejemplo2");
-        JMenuItem ejemplo3 = new JMenuItem("Ejemplo3");
-        // Add sub-menu items to the parent menus
-        opcionesMenu.add(ejemplo1);
-        opcionesMenu.add(ejemplo11);
-        editarMenu.add(ejemplo2);
-        ayudaMenu.add(ejemplo3);
-        menuBar.add(opcionesMenu);
-        menuBar.add(editarMenu);
-        menuBar.add(ayudaMenu);
-        ventana.setJMenuBar(menuBar);
-
-        // Create a panel for the folder selection area
-        JPanel folderSelectionPanel = new JPanel();
-        folderSelectionPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        JLabel folderLabel = new JLabel("Por favor, seleccione una carpeta:");
-        JButton selectButton = new JButton("Seleccionar");
-
-        selectButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int returnValue = fileChooser.showOpenDialog(null);
-                if (returnValue == JFileChooser.APPROVE_OPTION) {
-                    String selectedFolder = fileChooser.getSelectedFile().getAbsolutePath();
-                    folderLabel.setText("Carpeta seleccionada: " + selectedFolder);
-                }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new EditSoft().setVisible(true);
             }
         });
-        
-        folderSelectionPanel.add(folderLabel);
-        folderSelectionPanel.add(selectButton);
-        ventana.add(folderSelectionPanel, BorderLayout.NORTH);
-
-        // Create a JSplitPane
-        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-
-        // Create a TextArea with ScrollBar
-        JTextArea textArea = new JTextArea();
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        textArea.setPreferredSize(new Dimension(350, 0));
-
-        // Create a container on the right
-        JPanel container = new JPanel();
-        container.setPreferredSize(new Dimension(15, 0));
-
-        // Add components to the JSplitPane
-        splitPane.setLeftComponent(scrollPane);
-        splitPane.setRightComponent(container);
-
-        // Add the JSplitPane to the center of the content pane
-        ventana.add(splitPane, BorderLayout.CENTER);
-
-        ventana.setVisible(true);
     }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JButton jButton2;
+    private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea jTextArea1;
+    // End of variables declaration                   
 }
